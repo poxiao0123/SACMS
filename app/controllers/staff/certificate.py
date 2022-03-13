303dc5e2b385aed13cb78876cc8cbba073602373
poxiao <lwl_0123@yeah.net>
2022-03-13 23:30:04 +0800

    enter

--- app/controllers/staff/certificate.py
+++ app/controllers/staff/certificate.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from app.services import services
-from sanic import Blueprint, response
+from sanic import Blueprint
 from sanic.response import json
 from sanic_validation import validate_args, validate_json
+from this import d
 
 certificate = Blueprint("certificate", url_prefix="/staff/certificate")
 
 
-@certificate.get("/remove")
+@certificate.get("/remove")  # 移除证件 1
 @validate_args(
     {
         "c_id": {"type": "string", "required": True},
@@ -24,43 +25,71 @@ async def remove(request):
         return json({"code": -1, "msg": "删除失败"})
 
 
-@certificate.get("/list")
+@certificate.get("/list")  # 获取证件
 async def lists(request):
-    c_id = request.args.get("c_id", None)
-    c_name = request.args.get("c_name", None)
-    s_id = request.args.get("s_id", None)
-    res = await services.certificate.list(request, c_id, c_name, s_id)
-    if res:
-        data = {
-            "c_id": res[0],
-            "c_name": res[1],
-            "c_imgpath": res[2],
-            "s_id": res[3]
-        }
-        return json({"code": 0, "msg": "查询成功", "data": data})
+    c_id = request.args.get("c_id")
+    c_name = request.args.get("c_name")
+    s_id = request.args.get("s_id")
+    res = None
+    if c_id:
+        res = await services.certificate.list_by_c_id(request, c_id)
+        if res:
+            data = {
+                "c_id": res[0],
+                "c_name": res[1],
+                "c_imgpath": res[2],
+                "s_id": res[3]
+            }
+            return json({"code": 0, "msg": "查询成功", "data": data})
+        else:
+            return json({"code": -1, "msg": "查询失败"})
+    elif c_name:
+        res = await services.certificate.list_by_c_name(request, c_name)
+        if res:
+            # 初始化data数组
+            data = {
+                "c_id": [],
+                "c_imgpath": [],
+                "s_id": [],
+                "s_name": []
+            }
+            for item in res:
+                data["c_id"].append(item[0])
+                data["c_imgpath"].append(item[1])
+                data["s_id"].append(item[2])
+                data["s_name"].append(item[3])
+            print(data)
+            return json({"code": 0, "msg": "查询成功", "data": data})
+        else:
+            return json({"code": -1, "msg": "查询失败"})
+    elif s_id:
+        res = await services.certificate.list_by_s_id(request, s_id)
+        if res:
+            data = {
+                "c_id": res[0],
+                "c_name": res[1],
+                "c_imgpath": res[2],
+                "s_id": res[3]
+            }
+            return json({"code": 0, "msg": "查询成功", "data": data})
+        else:
+            return json({"code": -1, "msg": "查询失败"})
     else:
-        return json({"code": -1, "msg": "查询失败"})
+        return json({"code": -1, "msg": "关键字错误"})
 
 
-@certificate.post("/add")
+@certificate.post("/add")  # 添加证件 1
 async def add(request):
-    c_name = request.form.get("c_name")
-    s_id = request.form.get("s_id")
-    c_imgpath = "./static/{}_{}.jpg".format(s_id, c_name)
+    c_name = request.form.get("c_name")  # 获取证件名
+    s_id = request.form.get("s_id")  # 获取员工姓名
+    c_imgs = request.files.get("c_imgs")  # 获取证件照片
     row = {
         "c_name": c_name,
-        "s_id": s_id,
-        "c_imgpath": c_imgpath
+        "s_id": s_id
     }
-    res = await services.certificate.add(request, row)
-    if res:
-        return await response.file_stream(
-            c_imgpath,
-            chunk_size=1024,
-            mime_type="application/form-data"
-        )
-    else:
-        return json({"code": -1, "msg": "添加失败"})
+    res = await services.certificate.add(request, c_imgs, row)
+    return json({"code": 0, "msg": "上传成功"}) \
+        if res > 0 else json({"code": -1, "msg": "上传失败"})
 
 
 @certificate.post("/change")
--- app/services/services.py
+++ app/services/services.py
@@ -1,3 +1,5 @@
+import os
+
 from app.models.mysql import certificate, staff
 from sqlalchemy import select
 
@@ -60,9 +62,23 @@ class Staff:  # 员工类 1
 
 
 class Certificate:
-    async def add(self, request, row):  # 添加员工证件 
-        conn = request.app.ctx.db
+    async def add(self, request, c_imgs, row):  # 添加员工证件 1
         try:
+            # 1 创建图片目录
+            basepath = os.getcwd()
+            c_folder = os.path.join(basepath, "static", row["s_id"])
+            print("c_imgpath", "-" * 10, c_folder)
+            folder = os.path.exists(c_folder)
+            if not folder:
+                os.makedirs(c_folder)
+            img_ext = c_imgs.name.split('.')[-1]
+            c_imgpath = c_folder + "\\" + row["c_name"] + "." + img_ext
+            # 2 写入文件
+            with open(c_imgpath, "wb") as fw:
+                fw.write(c_imgs.body)
+                conn = request.app.ctx.db
+            # 3 写入数据库
+            row["c_imgpath"] = c_imgpath
             query = certificate.insert().values(**row)
             print(query)
             res = await conn.execute(query=query)
@@ -71,31 +87,30 @@ class Certificate:
             print(e)
             return False
 
-    async def remove(self, request, row):
+    async def remove(self, request, row):  # 删除员工证件 1
         conn = request.app.ctx.db
+        # 1 获取图片目录
         query = (
-            select(certificate.c.c_id)
+            select([certificate.c.c_imgpath])
             .select_from(certificate)
             .where(certificate.c.c_id == row["c_id"])
         )
-        print(query)
-        res = conn.fetch_all(query=query)
-        print(res)
-        # try:
-        #     query = (
-        #         certificate
-        #         .delete()
-        #         .where(certificate.c.c_id == row["c_id"])
-        #     )
-        #     print(query)
-        #     await conn.execute(query=query)
-        # except Exception as e:
-        #     print(e)
-        #     return False
-        # else:
-        #     return True
+        c_imgpath = list(await conn.fetch_one(query=query))[0]
+        # 2 删除数据
+        query = certificate.delete().where(certificate.c.c_id == row["c_id"])
+        res = await conn.execute(query=query)
+        if res > 0:
+            # 3 删除图片
+            try:
+                os.remove(c_imgpath)
+            except Exception as e:
+                print(e)
+                return False
+            return True
+        else:
+            return False
 
-    async def change(self, request, row):
+    async def change(self, request, row):  # 修改证件
         conn = request.app.ctx.db
         try:
             query = (
@@ -111,21 +126,41 @@ class Certificate:
         else:
             return True
 
-    async def list(self, request, c_id, c_name, s_id):
+    async def list_by_c_id(self, request, row):  # 按照证件id获取 1
         conn = request.app.ctx.db
-        row = None
-        if c_id is not None:
-            row = c_id
-        elif c_name is not None:
-            row = c_name
-        else:
-            row = s_id
         query = (
             select(["*"])
             .select_from(certificate)
             .where(certificate.c.c_id == row)
         )
-        print(query)
-        res = await conn.fetch_one(query=query)
+        res = list(await conn.fetch_one(query=query))
+        return res
+
+    async def list_by_c_name(self, request, row):  # 按照证件name获取
+        conn = request.app.ctx.db
+        query = (
+            r"select c_id, c_imgpath, s_id , name "
+            r"from certificate as c "
+            r"inner join staff as s "
+            r"on c.s_id = s.id "
+            "where c_name='{}'".format(row)
+        )
+        res = []
+        for r in await conn.fetch_all(query=query):
+            r = list(r)
+            res.append(r)
         print(res)
         return res
+
+    async def list_by_s_id(self, request, row):  # 按照员工id获取 1
+        conn = request.app.ctx.db
+        query = (
+            select(["*"])
+            .select_from(certificate)
+            .where(certificate.c.s_id == row)
+        )
+        res = list(await conn.fetch_all(query=query))
+        print(res)
+
+    async def getImg(self, request, row):  # 获取图片
+        return 0
